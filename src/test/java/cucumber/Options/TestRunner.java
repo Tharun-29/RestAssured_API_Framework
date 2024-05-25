e4bcd0634dfcbc00ab07fe8c6b27adbbaaf51ae4
Tharun-29 <tharun.subramani007@gmail.com>
2024-05-25 18:30:45 +0530

    Implementation of Cucumber HTML Report with Plugins

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinitions"})
+@CucumberOptions(features = "src/test/java/features/PlaceValidations.feature", glue = {"stepDefinitions"}, plugin = "json:target/jsonReports/cucumber-report.json")
 public class TestRunner {
   //tags = "@DeletePlace"
 }
