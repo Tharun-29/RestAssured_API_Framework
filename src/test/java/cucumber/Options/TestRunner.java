7e64d7877b4a4452c9b4b6071c57acb1076c81e3
Tharun-29 <tharun.subramani007@gmail.com>
2024-05-25 16:43:25 +0530

    Implementation of Tag Mechanism using Maven

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinitions"},tags = "@DeletePlace")
+@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinitions"})
 public class TestRunner {
-
+  //tags = "@DeletePlace"
 }
+	
--- target/test-classes/features/eCommerce.feature
+++
@@ -1,9 +0,0 @@
-Feature: Create Customer Product in eComm Application
-
-Scenario: Verify Customer is able to Create Product in eComm Application
-
-
-Given CreateProduct Payload is Triggered
-When customer creates order via CreateOrder Payload with Post Https Request
-Then the API Status is Success with response 200
-And "Product Added Successfully" product added success response is displayed
